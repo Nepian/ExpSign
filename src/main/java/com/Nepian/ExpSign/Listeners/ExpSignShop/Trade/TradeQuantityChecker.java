ab9be5dc3365ea7be6b9f5e610ab305c85f6ed0d
Nepian <nepia.ms@gmail.com>
2015-11-13 09:27:11 +0900

    Adminshop からの売買時に QuantityChecker で ShopType のチェックをしていなかった部分を修正

--- src/main/java/com/Nepian/ExpSign/Listeners/ExpSignShop/Trade/TradeQuantityChecker.java
+++ src/main/java/com/Nepian/ExpSign/Listeners/ExpSignShop/Trade/TradeQuantityChecker.java
@@ -1,61 +1,59 @@
 package com.Nepian.ExpSign.Listeners.ExpSignShop.Trade;
 
-import java.util.UUID;
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.Nepian.Breeze.Utils.PlayerUtil;
-import com.Nepian.ExpSign.Configuration.Properties;
 import com.Nepian.ExpSign.Events.ExpSignShopTradeEvent;
 import com.Nepian.ExpSign.Events.ExpSignShopTradeEvent.Outcome;
-import com.Nepian.ExpSign.Userdata.Userdata;
-import com.Nepian.ExpSign.Userdata.UserdataManager;
+import com.Nepian.ExpSign.Events.ExpSignShopTradeEvent.ShopType;
+import com.Nepian.ExpSign.Events.ExpSignShopTradeEvent.TradeType;
+import com.Nepian.ExpSign.ExpBank.ExpBank;
 
 public class TradeQuantityChecker implements Listener {
-	private static ExpSignShopTradeEvent event;
-	private static int quantity;
-	private static int playerExp;
 
 	@EventHandler(priority = EventPriority.NORMAL)
-	public static void onTrade(ExpSignShopTradeEvent eve) {
-		event = eve;
+	public static void onTradeBuy(ExpSignShopTradeEvent event) {
 
 		if (event.isCancelled()) {
 			return;
 		}
 
-		quantity = event.getQuantity();
-		playerExp = PlayerUtil.getExp(event.getPlayer());
-
-		switch (event.getTradeType()) {
-		case BUY:
-			buyQuantityCheck();
-			break;
-		case SELL:
-			sellQuantityCheck();
-			break;
+		if (event.getTradeType() != TradeType.BUY) {
+			return;
 		}
-	}
 
-	/* Private Methods ------------------------------------------------------*/
-
-	private static void buyQuantityCheck() {
-		final UUID uuid = PlayerUtil.getUUID(event.getOwnerName());
-		final Userdata userdata = UserdataManager.getUserdata(uuid);
-		final int ownerExp = userdata.getInteger(Properties.EXPBANK_PATH);
+		final int quantity = event.getQuantity();
+		final int playerExp = PlayerUtil.getExp(event.getPlayer());
 
 		if (PlayerUtil.MAX_EXP < playerExp + quantity) {
 			event.setOutcome(Outcome.MAX_EXP);
 		}
 
-		if (ownerExp - quantity < 0) {
-			event.setOutcome(Outcome.NO_OWNER_EXP);
+		if (event.getShopType() != ShopType.ADMIN) {
+			final int ownerExp = ExpBank.check(event.getOwnerName());
+
+			if (ownerExp - quantity < 0) {
+				event.setOutcome(Outcome.NO_OWNER_EXP);
+			}
 		}
 	}
 
-	private static void sellQuantityCheck() {
+	@EventHandler(priority = EventPriority.NORMAL)
+	public static void onTradeSell(ExpSignShopTradeEvent event) {
+
+		if (event.isCancelled()) {
+			return;
+		}
+
+		if (event.getTradeType() != TradeType.SELL) {
+			return;
+		}
+
+		final int quantity = event.getQuantity();
+		final int playerExp = PlayerUtil.getExp(event.getPlayer());
+
 		if (playerExp - quantity < 0) {
 			event.setOutcome(Outcome.NO_PLAYER_EXP);
 		}
