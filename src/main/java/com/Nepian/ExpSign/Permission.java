f08e2a565a4d353f5eba38dd187bc03ed946cde5
Nepian <nepia.ms@gmail.com>
2015-11-17 12:21:56 +0900

    Permission OTHER_NAME 削除

--- src/main/java/com/Nepian/ExpSign/Permission.java
+++ src/main/java/com/Nepian/ExpSign/Permission.java
@@ -1,7 +1,6 @@
 package com.Nepian.ExpSign;
 
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public enum Permission {
 	SHOP_BUY("ExpSign.shop.buy"),
@@ -9,7 +8,6 @@ public enum Permission {
 	SIGN_CREATION_BUY("NepianEXP.sign.create.buy"),
 	SIGN_CREATION_SELL("NepianEXP.sign.create.sell"),
 
-	OTHER_NAME("NepianEXP.name."),
 	ADMIN("NepianEXP.admin"),
 
 	NOFEE("NepianEXP.nofee");
@@ -30,14 +28,6 @@ public enum Permission {
 
 	public static boolean has(CommandSender sender, String node) {
 		return sender.hasPermission(node) || sender.hasPermission(node.toLowerCase());
-	}
-
-	public static boolean otherName(Player player, String name) {
-		if (has(player, Permission.ADMIN)) return false;
-
-		String node = OTHER_NAME + name;
-
-		return hasPermissionSet(player, node) || hasPermissionSet(player, node.toLowerCase());
 	}
 
 	public static boolean hasPermissionSet(CommandSender sender, String permission) {
