f657f2a2ea48d5b393c823f72bba4febfd03be97
Nepian <nepia.ms@gmail.com>
2015-11-17 12:17:26 +0900

    全体的にシステムを改善

--- src/main/java/com/Nepian/ExpSign/Configuration/Properties.java
+++
@@ -1,8 +0,0 @@
-package com.Nepian.ExpSign.Configuration;
-
-
-public class Properties {
-	public static String NAME_PATH = "Name";
-	public static String EXP_PATH = "Exp";
-	public static String EXPBANK_PATH = "ExpBank";
-}
